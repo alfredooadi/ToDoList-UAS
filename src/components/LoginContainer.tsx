30deb816cdf00d9bc3dbac9007122cd37c65d126
chillinsloth <leonardojosuan@gmail.com>
2021-11-29 22:36:20 +0700

    Perbaikan login register

--- src/components/LoginContainer.tsx
+++ src/components/LoginContainer.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import  { auth, db }  from '../firebaseConfig';
 import "./HeaderContainer.css";
 interface ContainerProps {}
 
 const LoginContainer: React.FC<ContainerProps> = () => {
+  const [loading, setLoading] = useState(false);
+  const [loginemail, setLoginemail] = useState("");
+  const [loginpassword, setLoginpassword] = useState("");
+
+  const handleLogin = async () => {
+    setLoading(true);
+    try {
+      const user = await signInWithEmailAndPassword(auth, loginemail, loginpassword);
+      console.log("yes");
+    } catch (e){
+      alert("Incorrect Email or Password!");
+      console.error(e);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="container">
       <br></br>
@@ -12,12 +30,12 @@ const LoginContainer: React.FC<ContainerProps> = () => {
       <br></br>
       <form autoComplete="off" className="form-group">
         <label>Email</label>
-        <input placeholder="Enter Email" type="email" className="form-control" required />
+        <input placeholder="Enter Email" type="email" className="form-control" required onChange={(e) => setLoginemail(e.target.value)} />
         <br></br>
         <label>Password</label>
-        <input placeholder="Enter Password" type="password" className="form-control" required />
+        <input placeholder="Enter Password" type="password" className="form-control" required onChange={(e) => setLoginpassword(e.target.value)} />
         <br></br>
-        <button type="submit" className="btn btn-success mybtn2">
+        <button disabled={loading} type="submit" className="btn btn-success mybtn2" onClick={handleLogin}>
           Login
         </button>
       </form>
--- src/components/RegisterContainer.tsx
+++ src/components/RegisterContainer.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore"
+import  { auth, db }  from '../firebaseConfig';
 import "./HeaderContainer.css";
 interface ContainerProps {}
 
@@ -8,10 +10,22 @@ const RegisterContainer: React.FC<ContainerProps> = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const [registError, setRegistError] = useState("");
 
-  const handleRegister = () => {};
+  const handleRegister = async () => {
+    setLoading(true);
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      await addDoc(collection(db, "users"), setDoc(doc(db, "users", user.user.uid), {
+        fullname : fullName
+      }));
+      console.log("Document written ID: ", user.user.uid);
+    } catch (e){
+      console.error(e);
+    }
+    setLoading(false);
+  };
 
   return (
     <div className="container">
@@ -19,17 +33,17 @@ const RegisterContainer: React.FC<ContainerProps> = () => {
       <br></br>
       <h2>Register</h2>
       <br></br>
-      <form autoComplete="off" className="form-group" onSubmit={handleRegister}>
+      <form autoComplete="off" className="form-group" >
         <label>Full Name</label>
-        <input placeholder="Enter Full Name" type="text" className="form-control" required onChange={(e) => setFullName(e.target.value)} value={fullName} />
+        <input placeholder="Enter Full Name" type="text" className="form-control" required onChange={(e) => setFullName(e.target.value)} />
         <br></br>
         <label>Email</label>
-        <input placeholder="Enter Email" type="email" className="form-control" required onChange={(e) => setEmail(e.target.value)} value={email} />
+        <input placeholder="Enter Email" type="email" className="form-control" required onChange={(e) => setEmail(e.target.value)} />
         <br></br>
         <label>Password</label>
-        <input placeholder="Enter Password" type="password" className="form-control" required onChange={(e) => setPassword(e.target.value)} value={password} />
+        <input placeholder="Enter Password" type="password" className="form-control" required onChange={(e) => setPassword(e.target.value)} />
         <br></br>
-        <button type="submit" className="btn btn-success mybtn2">
+        <button disabled={loading} type="submit" className="btn btn-success mybtn2" onClick={handleRegister}>
           Sign Up
         </button>
       </form>
